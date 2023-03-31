beca14e00ed1a054c993f17697ea9b7efffa491c
asif247 <86229363+asif247@users.noreply.github.com>
2023-03-31 16:36:01 +0600

    data added

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import "./Blogs.css";
+const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch('../../../public/data.json')
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+    return
+    <div>
+      
+  </div>;
+};
+
+export default Blogs;
